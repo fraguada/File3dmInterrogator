5c0d25b8c4acb9c6cfabb0b5aeb0b386ef9d94b2
Luis Fraguada <fraguada@gmail.com>
2017-09-22 21:12:09 +0200

    First commit.

---
+++ File3dm Interrogator/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace File3dmInterrogator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem_File = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem_FileOpen = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem_File});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(274, 42);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem_File
+            // 
+            this.toolStripMenuItem_File.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem_FileOpen});
+            this.toolStripMenuItem_File.Name = "toolStripMenuItem_File";
+            this.toolStripMenuItem_File.Size = new System.Drawing.Size(64, 38);
+            this.toolStripMenuItem_File.Text = "File";
+            // 
+            // toolStripMenuItem_FileOpen
+            // 
+            this.toolStripMenuItem_FileOpen.Name = "toolStripMenuItem_FileOpen";
+            this.toolStripMenuItem_FileOpen.Size = new System.Drawing.Size(173, 38);
+            this.toolStripMenuItem_FileOpen.Text = "Open";
+            this.toolStripMenuItem_FileOpen.Click += new System.EventHandler(this.ToolStripMenuItem_FileOpen_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Location = new System.Drawing.Point(0, 42);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(274, 187);
+            this.textBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(274, 229);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "File3dm Interrogator";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem_File;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem_FileOpen;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ File3dm Interrogator/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Text;
+using System.Windows.Forms;
+
+namespace File3dmInterrogator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ToolStripMenuItem_FileOpen_Click(object sender, EventArgs e)
+        {
+
+            OpenFileDialog openFileDialog1 = new OpenFileDialog()
+            {
+                InitialDirectory = "c:\\",
+                Filter = "3dm files (*.3dm)|*.3dm|All files (*.*)|*.*",
+                FilterIndex = 1,
+                RestoreDirectory = true
+            };
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+
+                Debug.WriteLine(openFileDialog1.FileName);
+
+                try
+                {
+                    
+                    var doc = Rhino.FileIO.File3dm.Read(openFileDialog1.FileName,Rhino.FileIO.File3dm.TableTypeFilter.Light, Rhino.FileIO.File3dm.ObjectTypeFilter.Any);
+
+                    
+
+                    var report = new StringBuilder();
+                    report.AppendLine(openFileDialog1.FileName);
+                    
+                    report.AppendLine("Rhino Version " + doc.ApplicationName);
+                    report.AppendLine("File Version " + doc.Revision.ToString());
+
+                    report.AppendLine(doc.Objects.Count.ToString() + " Objects in this file.");
+                 
+                    var objDictionary = new Dictionary<string, int>();
+
+
+                    
+                    foreach (var obj in doc.Objects)
+                    {
+                        Debug.WriteLine(obj.Geometry.ObjectType.ToString());
+                        Debug.WriteLine(obj.GetType().ToString());
+
+                        var id = obj.GetType().GetProperty("Id").GetValue(obj, null) as Guid?;
+
+                        Debug.WriteLine(id.ToString());
+
+                        if (objDictionary.ContainsKey(obj.Geometry.ObjectType.ToString()))
+                            objDictionary[obj.Geometry.ObjectType.ToString()] = objDictionary[obj.Geometry.ObjectType.ToString()]+1;
+                        else
+                            objDictionary.Add(obj.Geometry.ObjectType.ToString(), 1);
+
+                    }
+
+                    foreach (var kvp in objDictionary)
+                    {
+                        string line = kvp.Key + "\t" + kvp.Value.ToString();
+                        Debug.WriteLine(line);
+                        report.AppendLine(line);
+                    }
+
+                    textBox1.Text = report.ToString();
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+        }
+    }
+}
